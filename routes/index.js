e69d1bf11b128277305e71f5801e43d8f26d163c
Cheng Gong <cheng.gong@mail.mcgill.ca>
2014-09-20 04:37:08 -0400

    email function using sendGrid

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+
+var sendgrid  = require('sendgrid')('g-cheng', 'HackTheNorth');
+
 /* GET recording page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'QuickVid' });
@@ -11,6 +14,29 @@ router.get('/video', function(req, res) {
 	res.render('video', {
 	    "token" : req.query.token
 	});
+});
+
+// email
+router.post('/sendEmail', function(req, res) {
+
+    // Get our form values. These rely on the "name" attributes
+    var from = req.body.username;
+    var to = req.body.useremail;
+
+    console.log('button pressed');
+    var payload   = {
+	  to      :  to,
+	  from    :  from,
+	  subject : 'Saying Hi',
+	  text    : 'This is my first email through SendGrid'
+	}
+
+	sendgrid.send(payload, function(err, json) {
+	  if (err) { console.error(err); }
+	  console.log(json);
+	});
+
+
 });
 
 module.exports = router;
