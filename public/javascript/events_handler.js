352c61ca3faecf8fc68936172d4af68bb3a8ac32
mattmili <matthewmilitante@gmail.com>
2014-09-20 19:06:52 -0400

    added facebook and twitter buttons

--- public/javascript/events_handler.js
+++ public/javascript/events_handler.js
@@ -10,4 +10,8 @@ $(document).ready(function() {
 			"ziggeo-width": 176
 		}).css('margin-bottom', 25);
 	}
+
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+	(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
+
 });
