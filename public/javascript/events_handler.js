7a61f0c7c0972a2587c846d539cafd84edf86b2f
Cheng Gong <cheng.gong@mail.mcgill.ca>
2014-09-20 10:13:22 -0400

    the form will only be displayed once a video is recorded

--- public/javascript/events_handler.js
+++ public/javascript/events_handler.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	ZiggeoApi.Events.on("submitted", function (data) {
 		$("#tokenId").val(data.video.token);
+		var myForm =document.getElementById('formAddUser');
+		myForm.style.display='inline';
 	});
 });
