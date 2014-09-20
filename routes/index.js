425f76a4c16db58367d4cd6bcb45329f96f1311f
Ze-Q <ze.q.zhang@mail.mcgill.ca>
2014-09-20 04:20:08 -0400

    Added video page to display different videos.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET recording page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'QuickVid' });
+});
+
+/* GET video page. */
+router.get('/video', function(req, res) {
+	res.render('video', {
+	    "token" : req.query.token
+	});
 });
 
 module.exports = router;
